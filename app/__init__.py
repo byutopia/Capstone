f4478c370243e6b73d829e3e59e7474719fe7001
root <root@localhost.localdomain>
2017-11-18 11:58:33 -0700

    Fix spelling error

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #import modules from folder.filename
 from home.controller import home_mod as home_module
 from rainmachine.controller import rainmachine_mod as rainmachine_module
-frome purpleair.controller import purpleair_mod as purpleair_module
+from purpleair.controller import purpleair_mod as purpleair_module
 # Register blueprint
 app.register_blueprint(home_module)
 # app.register_blueprint(module name)
