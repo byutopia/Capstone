e8fd9fd94a720fddbaa31770ff69bb07c99c439c
Alexandra Bradford <rekushi.bradford@gmail.com>
2018-01-26 13:03:38 -0700

    Resolving merge changes and adding my changes.

---
+++ app/static/js/rainmachine.js
@@ -0,0 +1,32 @@
+function start(id) {
+	var url = '/rainmachine/start';
+	var data = {id: id};
+
+	fetch(url, {
+	  method: 'POST', // or 'PUT'
+	  body: JSON.stringify(data),
+	  headers: new Headers({
+	    'Content-Type': 'application/json'
+	  })
+	}).then(res => res.json())
+	.catch(error => console.error('Error:', error))
+	.then(response => console.log('Success:', response));
+	console.log(id);
+}
+
+
+function stop(id) {
+	var url = '/rainmachine/stop';
+	var data = {id: id};
+
+	fetch(url, {
+	  method: 'POST', // or 'PUT'
+	  body: JSON.stringify(data),
+	  headers: new Headers({
+	    'Content-Type': 'application/json'
+	  })
+	}).then(res => res.json())
+	.catch(error => console.error('Error:', error))
+	.then(response => console.log('Success:', response));
+	console.log(id);
+}
