954f5bcdf28594c17e543fd92f447ffd60835bae
Alexandra Bradford <rekushi.bradford@gmail.com>
2018-03-05 16:01:55 -0700

    Partial implementation of login features.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, Response, render_template, send_from_directory
+from flask import session, redirect, url_for, escape, request
 from flask_socketio import SocketIO
+from hashlib import md5
 import json
-
+import MySQLdb
 
 socketio = SocketIO()
+
 
 def create_app(debug=False):
     app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -20,10 +23,54 @@ def create_app(debug=False):
     app.register_blueprint(wago_module)
     app.register_blueprint(camera_module)
 
+    db = MySQLdb.connect(host="localhost", user="admin", passwd="strong password", db="capstone")
+    cur = db.cursor()
+
     import jinja2.exceptions
+
+    class ServerError(Exception):pass
+
     @app.route('/')
     def index():
-        return render_template('index.html')
+        if 'username' in session:
+            return redirect(url_for('login'))
+
+    username_session = escape(session['username']).capitalize()
+    return render_template('index.html', session_user_name=username_session)
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if 'username' in session:
+            return redirect(url_for('index'))
+
+            error = None
+            try:
+                if request.method == 'POST':
+                    username_form  = request.form['username']
+                    cur.execute("SELECT COUNT(1) FROM users WHERE name = {};"
+                            .format(username_form))
+
+                if not cur.fetchone()[0]:
+                    raise ServerError('Invalid username')
+                    password_form  = request.form['password']
+                    cur.execute("SELECT pass FROM users WHERE name = {};"
+                            .format(username_form))
+
+                for row in cur.fetchall():
+                    if md5(password_form).hexdigest() == row[0]:
+                        session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+
+                raise ServerError('Invalid password')
+            except ServerError as e:
+                error = str(e)
+
+    return render_template('login.html', error=error)
+
+    @app.route('/logout')
+    def logout():
+        session.pop('username', None)
+        return redirect(url_for('index'))
 
     @app.route('/<pagename>')
     def admin(pagename):
