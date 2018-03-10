f5c08c833e86d4bc5ec3ae3677747e25fe5c329a
Cameron Meherg <cameron.meherg@gmail.com>
2018-03-10 09:53:18 -0800

    Dynamically get longitude and latitude from a list of ips to make the array for the map

--- app/modules/purpleair/controller.py
+++ app/modules/purpleair/controller.py
@@ -1,8 +1,31 @@
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
-
+import requests
 purpleair_mod = Blueprint('purpleair', __name__)
 #set a route
-purpleairDevices = [{'ip':"192.168.20.61", 'lat':40.315155, 'lon':-111.666672 },{'ip':'192.168.20.59','lat':40.324207,'lon':	-111.715004}]
+purpleairIPs = ['192.168.20.61','192.168.20.59']
+
+session = requests.Session()
+
+def getCordsByIP(IPs):
+	cordsAndIP = []
+	URL = "http://{}/json"
+	for ip in IPs:
+		try:
+			url = URL.format(ip)
+			req = session.get(url, timeout=3, verify=False)
+			res = req.json()
+			lat = res['lat']
+			lon = res['lon']
+			cordsAndIP.append({'ip':ip,'lat':lat, 'lon': lon})
+		except ValueError:
+			return{"error": 2, "message": "Error reading received JSON object"}
+		except requests.exceptions.RequestException:
+			print "Error: could not connect to IP "
+			print ip
+	return cordsAndIP
+
+purpleairDevices = getCordsByIP(purpleairIPs)
+print purpleairDevices
 @purpleair_mod.route('/purpleair', methods = ['GET'])
 def purpleair():
 	#return the template
