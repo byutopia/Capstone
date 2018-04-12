4219bfee18f3cc7e835746869b6f828e8697f540
Aleks <achristensen@byu.edu>
2018-04-12 23:07:12 +0000

    Fix Wago lightbulb relativity and scroll.

--- utopia.py
+++ utopia.py
@@ -5,4 +5,4 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True, log_output=True)
+    socketio.run(app, host='0.0.0.0', port=80, debug=True, log_output=True)
